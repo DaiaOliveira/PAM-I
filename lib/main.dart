968b529618d2a45cb9c8a6a58d5570c8d8ae9baf
Daia <daianecoliveira2@gmail.com>
2024-08-20 20:37:39 -0300

    branch com o código da aula do dia Tue, Aug 20, 2024  8:37:39 PM

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+String sorteia() {
+  final random = Random();
+  return random.nextInt(100).toString();
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final tema = Theme.of(context);
+    final bgColor = tema.colorScheme.inversePrimary;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        backgroundColor: bgColor,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Clique para gerar um número aleatório"),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Número sorteado: ${sorteia()}"),
+                  ),
+                );
+              },
+              child: const Text("Sortear"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Aplicacao extends StatelessWidget {
+  const Aplicacao({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Aula03",
+      home: HomePage(),
+    );
+  }
+}
+
+void main() => runApp(const Aplicacao());
